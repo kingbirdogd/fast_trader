c3e4a8d275d6f064f86211bdfa52e76b9fde50e0
David AU <kingbirdogd@gmail.com>
2020-05-02 04:51:37 +0800

    fixed compile bugs

--- core/server/src/server.cpp
+++ core/server/src/server.cpp
@@ -49,7 +49,7 @@ void server::start()
 		{
 #ifndef __APPLE__
 			cpu_set_t cpuset;
-			CPU_SET(_cpu_id, &cpuset);
+			CPU_SET(_cpu_core, &cpuset);
 			::pthread_setaffinity_np(self, sizeof(cpu_set_t), &cpuset);
 #endif
 		}
