1261ad58a0b7f82f71b6819ca2f4bd7332fe11cf
leo <leo@leo-PC>
2020-05-26 15:24:50 +0800

    1

--- top_client/src/top_client.cpp
+++ top_client/src/top_client.cpp
@@ -221,7 +221,9 @@ dbp::top::enhance_order top_client::new_order
 			if (turnover > _buy_power)
 			{
 				report.order_id = 0;
-				report.reject_reason = "exist buy power";
+				report.reject_reason = "exist buy power                                                                                 ";
+				//"exist buy power                                                                                 "
+				//"                                                                                                "
 				return report;
 			}
 			else
