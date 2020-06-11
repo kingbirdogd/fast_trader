ed86407088099992194d761b4cded6df6520d797
David AU <kingbirdogd@gmail.com>
2020-06-11 17:32:54 +0800

    fixed get_YYYYMMDDHHMMSSsss

--- inc/tools.h
+++ inc/tools.h
@@ -37,13 +37,13 @@ namespace dbp
 				time_t t = nano / 1000;
 				struct tm sTm;
 				localtime_r(&t, &sTm);
-				unsigned long long result = (sTm.tm_year + 1900) * 10000000000000
-						+ (sTm.tm_mon + 1) * 100000000000
-						+ sTm.tm_mday * 1000000000
-						+ sTm.tm_hour * 10000000
-						+ sTm.tm_min * 100000
-						+ sTm.tm_sec * 1000
-						+ sss;
+				unsigned long long result = (sTm.tm_year + 1900) * 10000000000000;
+				result += (sTm.tm_mon + 1) * 100000000000;
+				result += static_cast<unsigned long long>(sTm.tm_mday) * 1000000000;
+				result += static_cast<unsigned long long>(sTm.tm_hour) * 10000000;
+				result += static_cast<unsigned long long>(sTm.tm_min) * 100000;
+				result += static_cast<unsigned long long>(sTm.tm_sec) * 1000;
+				result += sss;
 				return result;
 			}
 			inline unsigned long long get_YYYYMMDDHHMMSSsss()
