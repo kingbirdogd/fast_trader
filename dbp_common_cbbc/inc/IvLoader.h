70d93d18032e772c13a994706645c213f149bd45
leo <leo@leo-PC>
2020-07-08 14:47:54 +0800

    1

--- dbp_common_cbbc/inc/IvLoader.h
+++ dbp_common_cbbc/inc/IvLoader.h
@@ -45,11 +45,11 @@ public:
 	virtual ~IvLoader();
 	void load(const char* filename);
 	vector<string> split(string str, char delimiter);
-	WarrantIv allWarrants();
+	vector<WarrantIv> allWarrants();
 	WarrantIv getWarrantIv(unsigned int);
 	int getUnderlyingCount();
 	int getWarrantCount();
-	WarrantIv getWarrantIvByIssuer(string ,unsigned int);
+	vector<WarrantIv> getWarrantIvByIssuer(string ,unsigned int);
 };
 
 
