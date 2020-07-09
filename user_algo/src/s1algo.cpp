3d9b0cf4be59a3fc4067c04f0347d38f282763a2
leo <leo@leo-PC>
2020-07-09 10:24:06 +0800

    1

--- user_algo/src/s1algo.cpp
+++ user_algo/src/s1algo.cpp
@@ -32,8 +32,8 @@ void s1algo::on_omdc_book(const Tradable& tradable)
 					obs->DetectedAsk = best_ask_price;
 
 					vector<warrant*> selectedWarrant = getSelectedWarrantFromMarketByIssuer("MB",tradable.m_Code, best_bid_price,best_ask_price );
-					for(unsigned int i=0; i<selectedWarrantV.size(); i++){
-						warrant* w = selectedWarrantV[i];
+					for(unsigned int i=0; i<selectedWarrant.size(); i++){
+						warrant* w = selectedWarrant[i];
 
 						w->Status = STATUS_READY;
 						obs->addWarrantOrCbbc(w);
