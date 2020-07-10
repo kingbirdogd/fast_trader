ab588b27405c5b9dc6e2d873f3b5a0765f63c898
leo <leo@leo-PC>
2020-07-10 14:47:38 +0800

    1

--- user_algo/inc/s1algo.hpp
+++ user_algo/inc/s1algo.hpp
@@ -43,7 +43,7 @@ public:
 
 	virtual vector<warrant*> getSelectedWarrantFromMarketByIssuer(std::string issuercode, unsigned int underlying, unsigned long long ubid, unsigned long long uask);
 
-	virtual bool doWarrantAction(std::string action, unsigned char side, unsigned long long price, unsigned long long quantity);
+	virtual bool doWarrantAction(warrant* w, unsigned char side, unsigned long long price, unsigned long long quantity);
 
 	virtual void on_omdc_book(const Tradable&);
 	virtual void on_omdd_book(const Tradable&);
