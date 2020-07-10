a680fc771e087157eaafb0197a3cb4a10f6e33cd
leo <leo@leo-PC>
2020-07-10 15:56:14 +0800

    1

--- smfh_srv/inc/smfh_srv_channel.h
+++ smfh_srv/inc/smfh_srv_channel.h
@@ -281,7 +281,7 @@ public:
 																}
 																else
 																{
-																	unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
+																	//unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
 																	//flush_printf("tm:%llu, Handle Refresh 2 , ChannelId:%u , mType: %u Code:%u\n", dbp::tools::srv::current(), _channel.m_uChannelId, mType, ucode);
 																	_Handler(OMD_GET_POINTER(pszBuffer, 0, dbp::omd::COmdMsgHeader), 0);
 																}
