d82d8e4f23199a9746a51f6a467215d2ab555de7
leo <leo@leo-PC>
2020-07-16 12:04:47 +0800

    1

--- user_algo/src/s1algo.cpp
+++ user_algo/src/s1algo.cpp
@@ -19,7 +19,7 @@ s1algo::s1algo(user& u, const std::string& name):
 	MarketStatus = MARKET_START;
 
 	vector<WarrantIv> allW = ivLoader.allWarrants();
-	for(unsigned int i=0; i<obsw.size(); i++){
+	for(unsigned int i=0; i<allW.size(); i++){
 		warrantPriceMap[allW[i]->Code] = new priceinfo();
 		warrantPriceMap[allW[i]->Code].Bestbid = 0;
 		warrantPriceMap[allW[i]->Code].Bestask = 0;
