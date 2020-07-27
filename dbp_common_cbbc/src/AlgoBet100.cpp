d4c3c482b9d8e2b2b8762c87ccc185b27282db26
BCH10I72 <BCH10I72@DESKTOP-RLO60S6>
2020-07-27 17:37:02 +0800

    1

---
+++ dbp_common_cbbc/src/AlgoBet100.cpp
@@ -0,0 +1,51 @@
+/*
+ * AlgoBet100.cpp
+ *
+ *  Created on: 2017¦~9¤ë4¤é
+ *      Author: leo
+ */
+
+#include "AlgoBet100.h"
+
+
+
+AlgoBet100::AlgoBet100() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AlgoBet100::~AlgoBet100() {
+	// TODO Auto-generated destructor stub
+}
+
+AlgoBet* AlgoBet100::getCurrentBet(string){
+	return this;
+}
+
+void AlgoBet100::selectBet(string){
+
+}
+
+unsigned long long AlgoBet100::fixQuantity(unsigned long long price, unsigned long long quantity){
+	return quantity;
+}
+
+unsigned long long AlgoBet100::fixQuantityByIssuer(unsigned long long ,unsigned long long quantity, string ){
+	return quantity;
+}
+
+unsigned long long AlgoBet100::fixQuantityBySpread(unsigned long long price, unsigned long long quantity, unsigned long long spread){
+
+	int noofspread = static_cast<int>(spread / 100000ull);
+
+	unsigned long long qty = static_cast<unsigned long long>(100000/noofspread);
+
+	unsigned long long betsize = static_cast<unsigned long long>((int)(qty/quantity + 0.1))*quantity;
+
+	if(betsize < quantity){
+		betsize = quantity;
+	}
+	return betsize;
+}
+
+
