cc8926d352b3219429e875dfbfb7e41b6ce96ede
leo <leo@leo-PC>
2020-10-06 15:56:19 +0800

    1

--- user_algo/src/csalgo.cpp
+++ user_algo/src/csalgo.cpp
@@ -234,11 +234,11 @@ void csalgo::on_omdc_book(const Tradable& tradable)
 
 				unsigned long long swidth = best_ask_price - best_bid_price;
 
-/*
+
 				auto selectwit = selectedWarrant.find(code);
 				if(selectwit == selectedWarrant.end())
 					return;
-*/
+
 
 				warrant* newWarrant = new warrant;
 				newWarrant->Date = DateUtil::getToday();
