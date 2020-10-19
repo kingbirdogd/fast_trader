893aa611b464ded3a7925a7f679fc8b0cfbd86e9
leo <leo@leo-PC>
2020-10-19 15:17:22 +0800

    1

--- user_algo/inc/bear.hpp
+++ user_algo/inc/bear.hpp
@@ -876,6 +876,8 @@ private:
 				_PriceInfoU->PFBestask =  uprice->PTBestask;
 */
 
+
+				/*
 				Log(std::string(" WCODE ") + std::to_string(code) +
 				std::string(" CODE ") + std::to_string(_warrant_code) +
 				std::string(" bid = ") + std::to_string(best_bid_price) +
@@ -889,7 +891,7 @@ private:
 				std::string(" FBestask = ") + std::to_string(_PriceInfoU->FBestask) +
 				std::string(" PFBestbid = ") + std::to_string(_PriceInfoU->PFBestbid) +
 				std::string(" PFBestask = ") + std::to_string(_PriceInfoU->PFBestask)
-								);
+								);*/
 
 
 				unsigned long buyin = _CbbcPriceMark->buyIn(best_ask_price);
