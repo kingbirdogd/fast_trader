68a9f2d28b2ffc7808e12a8a6319eb393722d741
leo <leo@leo-PC>
2020-10-21 12:29:00 +0800

    1

--- smfh_srv/inc/smfh_srv_channel.h
+++ smfh_srv/inc/smfh_srv_channel.h
@@ -274,14 +274,14 @@ public:
 															}
 															else if (bStartRefresh)
 															{
-																unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
-																flush_printf("tm:%llu, Handle Refresh 1 , ChannelId:%u , mType: %u Code:%u\n", dbp::tools::srv::current(), _channel.m_uChannelId, mType, ucode);
+																//unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
+																//flush_printf("tm:%llu, Handle Refresh 1 , ChannelId:%u , mType: %u Code:%u\n", dbp::tools::srv::current(), _channel.m_uChannelId, mType, ucode);
 																HANDLE(OMD_GET_POINTER(pszBuffer, 0, dbp::omd::COmdMsgHeader), 0, _channel.m_uChannelId);
 															}
 															else
 															{
-																//unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
-																//flush_printf("tm:%llu, ignore Refresh 1 , ChannelId:%u , mType: %u Code:%u\n", dbp::tools::srv::current(), _channel.m_uChannelId, mType, ucode);
+																unsigned int ucode = OMD_GET_VALUE(pszBuffer, 4, unsigned int);
+																flush_printf("tm:%llu, ignore Refresh 1 , ChannelId:%u , mType: %u Code:%u\n", dbp::tools::srv::current(), _channel.m_uChannelId, mType, ucode);
 															}
 															pszBuffer += OMD_GET_VALUE(pszBuffer, 0, unsigned short int);
 														}
