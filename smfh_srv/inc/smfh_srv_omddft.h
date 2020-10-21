e764057c92e31e35de862017c77ca2073a242e9f
leo <leo@leo-PC>
2020-10-21 09:31:48 +0800

    1

--- smfh_srv/inc/smfh_srv_omddft.h
+++ smfh_srv/inc/smfh_srv_omddft.h
@@ -10,22 +10,6 @@ inline static void handleOmddFt(dbp::omd::COmdMsgHeader* _pMsg, unsigned long lo
 #endif
 {
 	unsigned int uSecurityCode = OMD_GET_VALUE(_pMsg, 4, unsigned int);
-	/*
-	auto it_underlyging = codeTounderlying.find(uSecurityCode);
-	if (codeTounderlying.end() == it_underlyging)
-	{
-		return;
-	}
-	const auto& underlying = it_underlyging->second;
-	if (4 != underlying.InstrumentGroup)
-	{
-		return;
-	}
-	if (4001 != underlying.CommodityCode && 4002 != underlying.CommodityCode)
-	{
-		return;
-	}
-	*/
 	auto it = omddMap.find(uSecurityCode);
 	if (omddMap.end() == it)
 	{
@@ -181,6 +165,6 @@ inline static void handleOmddFt(dbp::omd::COmdMsgHeader* _pMsg, unsigned long lo
 
 }
 
-
+#endif
 
 
