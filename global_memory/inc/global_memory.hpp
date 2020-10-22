8fad35c73c760542795096b5f45721f37cfb572e
leo <leo@leo-PC>
2020-10-22 09:45:42 +0800

    1

--- global_memory/inc/global_memory.hpp
+++ global_memory/inc/global_memory.hpp
@@ -140,7 +140,7 @@ public:
 			it->second.quantity += quantity;
 			++it->second.number_of_order;
 
-			DEBUG("tm:%llu, OMDD Add Bid, AddBestBid: %d, Iterator Price: %d, Begin Bid Price: %d bidqty %ull, Begin Ask Price: %d askqty %ull\n",
+			DEBUG("tm:%llu, OMDD Add Bid, AddBestBid: %d, Iterator Price: %d, Begin Bid Price: %d bidqty %llu, Begin Ask Price: %d askqty %llu\n",
 							dbp::tools::srv::current(),
 							price, it->first, Bids.begin()->first,Bids.begin()->second.quantity, Asks.begin()->first, Asks.begin()->second.quantity);
 
@@ -157,7 +157,7 @@ public:
 			it->second.quantity += quantity;
 			++it->second.number_of_order;
 
-			DEBUG("tm:%llu, OMDD Add Ask, AddBestAsk: %d, Iterator Price: %d, Begin Bids Price: %d bidqty %ull, Begin Ask Price: %d askqty %ull\n",
+			DEBUG("tm:%llu, OMDD Add Ask, AddBestAsk: %d, Iterator Price: %d, Begin Bids Price: %d bidqty %llu, Begin Ask Price: %d askqty %llu\n",
 										dbp::tools::srv::current(),
 										price, it->first, Bids.begin()->first, Bids.begin()->second.quantity, Asks.begin()->first, Asks.begin()->second.quantity);
 
