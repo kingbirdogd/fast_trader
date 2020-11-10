752c37215991d5bad59177d5468fdb4ccc914749
BCH10I72 <BCH10I72@DESKTOP-RLO60S6>
2020-11-10 23:01:31 +0800

    1

--- smfh_srv/inc/smfh_srv_channel.h
+++ smfh_srv/inc/smfh_srv_channel.h
@@ -576,7 +576,9 @@ public:
 										event = { .events = EPOLLIN, .data = { .ptr = _channel.m_zfRefresh } };
 										zf_muxer_add(_channel.m_zfMuxer, zfur_to_waitable(_channel.m_zfRefresh), &event);
 #endif
+#ifndef NOT_MEASURE
 										uCnt = 0;
+#endif
 										uSum = 0;
 										CLEAR();
 										break;
