01f7e1b52dbdc64b5bfab1517a3962cbe6a1a77b
leo <leo@leo-PC>
2020-11-16 13:30:05 +0800

    1

--- dbp_common_cbbc/src/AlgoBet100.cpp
+++ dbp_common_cbbc/src/AlgoBet100.cpp
@@ -30,16 +30,62 @@ string AlgoBet100::selectBet(string){
 	return "Bet100";
 }
 
-unsigned long long AlgoBet100::fixQuantity(unsigned long long, unsigned long long quantity){
-	return quantity;
+unsigned long long AlgoBet100::fixQuantity(unsigned long long price, unsigned long long quantity){
+	unsigned long long betsize = quantity;
+		if(price >= 400000000){
+			betsize = 2500;
+		}
+		if(price >= 100000000 && price < 400000000){
+			betsize = 10000;
+		}
+		if(price >= 50000000 && price < 100000000){
+			betsize = 14000;
+		}
+		if(price >= 25000000 && price < 50000000){
+			betsize = 20000;
+		}
+		if(price < 25000000){
+			betsize = 100000;
+		}
+
+		betsize = static_cast<unsigned long long>((int)(betsize/quantity + 0.1))*quantity;
+
+		if(betsize < quantity){
+			betsize = quantity;
+		}
+
+		return betsize;
 }
 
-unsigned long long AlgoBet100::fixQuantityByIssuer(unsigned long long ,unsigned long long quantity, string ){
-	return quantity;
+unsigned long long AlgoBet100::fixQuantityByIssuer(unsigned long long price,unsigned long long quantity, string ){
+	unsigned long long betsize = quantity;
+		if(price >= 400000000){
+			betsize = 2500;
+		}
+		if(price >= 100000000 && price < 400000000){
+			betsize = 10000;
+		}
+		if(price >= 50000000 && price < 100000000){
+			betsize = 14000;
+		}
+		if(price >= 25000000 && price < 50000000){
+			betsize = 20000;
+		}
+		if(price < 25000000){
+			betsize = 100000;
+		}
+
+		betsize = static_cast<unsigned long long>((int)(betsize/quantity + 0.1))*quantity;
+
+		if(betsize < quantity){
+			betsize = quantity;
+		}
+
+		return betsize;
 }
 
-unsigned long long AlgoBet100::fixQuantityBySpread(unsigned long long, unsigned long long quantity, unsigned long long spread){
-
+unsigned long long AlgoBet100::fixQuantityBySpread(unsigned long long price, unsigned long long quantity, unsigned long long ){
+	/*
 	int noofspread = static_cast<int>(spread / 100000ull);
 
 	unsigned long long qty = static_cast<unsigned long long>(100000/noofspread);
@@ -49,6 +95,31 @@ unsigned long long AlgoBet100::fixQuantityBySpread(unsigned long long, unsigned 
 	if(betsize < quantity){
 		betsize = quantity;
 	}
+	return betsize;
+	*/
+	unsigned long long betsize = quantity;
+	if(price >= 400000000){
+		betsize = 2500;
+	}
+	if(price >= 100000000 && price < 400000000){
+		betsize = 10000;
+	}
+	if(price >= 50000000 && price < 100000000){
+		betsize = 14000;
+	}
+	if(price >= 25000000 && price < 50000000){
+		betsize = 20000;
+	}
+	if(price < 25000000){
+		betsize = 100000;
+	}
+
+	betsize = static_cast<unsigned long long>((int)(betsize/quantity + 0.1))*quantity;
+
+	if(betsize < quantity){
+		betsize = quantity;
+	}
+
 	return betsize;
 }
 
