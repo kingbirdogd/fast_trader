3e1e068a6b3c6152614ea2d2445b2d3e7941b215
leo <leo@leo-PC>
2021-10-22 09:22:48 +0800

    1

--- static_algos/src/bear.cpp
+++ static_algos/src/bear.cpp
@@ -682,14 +682,14 @@ void bear::on_omdd_trade(const Tradable& tradable)
 					if(p->getWtype() == BULL){
 						if((p->getLvlBid() == trade_price && p->isLevel()) || (p->getSellOut() == trade_price) || (best_bid_price > p->getSellOut() && p->getSellOut() > best_bid_price1))
 						{
-							if(sparam->isready && sparam->bullbuy &&  (  trade_price > sparam->bullr1s  && trade_price < sparam->bullr1e )){
+							if(sparam->isReady && sparam->bullbuy &&  (  trade_price > sparam->bullr1s  && trade_price < sparam->bullr1e )){
 								p->on_bull_trade(tradable);
 							}
 						}
 					}else{
 						if(p->getBuyIn() == trade_price && (p->status() == STATUS_READY || p->status() == STATUS_DONE) && p->action_status() == STAGE_START)
 						{
-							if(sparam->isready && sparam->bearbuy &&  ( sparam->bearr1s > trade_price && trade_price > sparam->bearr1e )){
+							if(sparam->isReady && sparam->bearbuy &&  ( sparam->bearr1s > trade_price && trade_price > sparam->bearr1e )){
 								p->on_bear_trade(tradable);
 							}
 						}
@@ -743,14 +743,14 @@ void bear::on_omdd_trade(const Tradable& tradable)
 						//Log(" Code = " + to_string(tradable.m_Code) + " Trade Price = " + to_string(trade_price) + " Buyin = " + to_string(p->getBuyIn()) + " sTATUS = " + to_string(p->status()));
 						if(p->getBuyIn() == trade_price  && (p->status() == STATUS_READY || p->status() == STATUS_DONE) && p->action_status() == STAGE_START)
 						{
-							if(sparam->isready && sparam->bullbuy &&  ( trade_price > sparam->bullr1s && trade_price < sparam->bullr1e )){
+							if(sparam->isReady && sparam->bullbuy &&  ( trade_price > sparam->bullr1s && trade_price < sparam->bullr1e )){
 								p->on_bull_trade(tradable);
 							}
 						}
 					}else{
 						if((p->getLvlBid() == trade_price && p->isLevel()) || (p->getSellOut() == trade_price) || (best_ask_price < p->getSellOut() && p->getSellOut() < best_ask_price1))
 						{
-							if(sparam->isready && sparam->bearbuy &&  ( sparam->bearr1s > trade_price && trade_price > sparam->bearr1e )){
+							if(sparam->isReady && sparam->bearbuy &&  ( sparam->bearr1s > trade_price && trade_price > sparam->bearr1e )){
 								p->on_bear_trade(tradable);
 							}
 						}
